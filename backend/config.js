06f354b1294e1f51c8c9f16a9a775f49f7da88fb
Štěpán <otrubastepan07@gmail.com>
2026-04-17 11:02:54 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -9,7 +9,7 @@ const config = {
     source: {
         pagePath: '/index.html',
         // pageUrl: 'http://localhost:3000/index.html'
-        pageUrl: 'https://gfmlztjjmhgadxbzcgnl.supabase.co/storage/v1/object/public/components_assets/krackonos_meme.webp'
+        pageUrl: 'https://hranice.link'
     },
     display: {
         width: 800,
